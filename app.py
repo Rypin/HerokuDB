db907030e0a64e388652354200785337ebf53b0b
Ryan P <ryanpineyro@gmail.com>
2020-02-09 21:41:40 -0500

    whatever

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request,redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -12,9 +12,20 @@ db_password = "0f737abe099ffc9dcc4d72375fa40bcbf1c1a5ca516ddc0040268f0e63edb50d"
 db_conn = psycopg2.connect(host = db_host, port= db_port, dbname=db_name, user=db_user, password=db_password)
 db_cursor = db_conn.cursor
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_fname = request.form['Fname']
+        task_lname = request.form['Lname']
+        task = "INSERT INTO users(firstname, lastname) VALUES ("+task_fname+", "+task_lname+")"
+        try:
+            db_cursor.execute(task)
+            return redirect('/')
+        except:
+            return 'Issue inserting into Database'
+    else:
+
+        return render_template('index.html')
 @app.route('/page2')
 def page2():
     return 'Welcome'
